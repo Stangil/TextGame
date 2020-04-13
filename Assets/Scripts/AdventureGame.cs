1cd8947089d6fc4a5c317ef19214b99159a8e03f
Stangil <stangil44@gmail.com>
2020-04-13 16:15:54 -0400

    Added scripts

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+   
+    void Start()
+    {
+        textComponent.text = "You are a dog, and you wake up in the kitchen, do you: \n"+
+            "Look in your bowl \n"+
+            "Look for your owner";
+    }
+
+    void Update()
+    {
+        
+    }
+}
