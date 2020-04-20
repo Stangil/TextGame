c28d1fc9bbeec34ba5e4375841b296acc4b4e944
Stangil <stangil44@gmail.com>
2020-04-20 15:33:45 -0400

    Added stuff

--- Assets/Scripts/AdventureGame.cs
+++ Assets/Scripts/AdventureGame.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,16 +7,36 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour
 {
     [SerializeField] Text textComponent;
-   
+    [SerializeField] State startingState;
+
+    State state;
     void Start()
+        
     {
-        textComponent.text = "You are a dog, and you wake up in the kitchen, do you: \n"+
-            "Look in your bowl \n"+
-            "Look for your owner";
+        state = startingState;
+        textComponent.text = state.GetStateStory();
     }
 
     void Update()
     {
-        
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+        textComponent.text = state.GetStateStory();
     }
 }
